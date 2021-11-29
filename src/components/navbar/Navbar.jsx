e45a10b2598e8f0b465d8e18d03a50885ffb1881
lauraAlabau <arlaura3@gmail.com>
2021-11-29 13:20:18 +0100

    Added global styles and installed material ui

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './navbar.scss'
+import React from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
-    return (
-        <div id="navbar">
+  return (
+    <div id="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Laura Alabau</a>
+          <div className="itemContainer">
 
+          </div>
         </div>
-    )
-}
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
